0b47436b9616cd988d561717fadd2a7597e2f32c
HydraHail <8326343+Hydrahail-Johnson@users.noreply.github.com>
2021-06-12 21:42:19 +0800

    Update start.py

--- jbot/bot/start.py
+++ jbot/bot/start.py
@@ -10,16 +10,17 @@ async def mystart(event):
     /start 开始使用本程序。
     /a 使用你的自定义快捷按钮。
     /bean 获取京豆变化，默认为总京豆收支。/bean in 京豆进账，/bean out 京豆支出。
-    /chart 获取京豆变化数据柱状图和曲线图。例，/chart 1，获取账号1的京豆变化。
-    /cmd 执行cmd命令,例如/cmd python3 /python/bot.py 则将执行python目录下的bot.py 不建议使用机器人使用并发，可能产生不明原因的崩溃。 
-    /edit 从jd目录下选择文件编辑，需要将编辑好信息全部发给机器人，机器人会根据你发的信息进行替换。建议用来编辑config或crontab.list 其他文件慎用！！！
-    /getcookie 扫码获取cookie 增加30s内取消按钮，30s后不能进行其他交互直到2分钟或获取到cookie。
-    /getfile 获取jd目录下文件。
-    /log 选择查看执行日志。
-    /node 执行js脚本文件，直接输入/node jd_bean_change 如执行其他自己js，需输入绝对路径。即可进行执行。该命令会等待脚本执行完，期间不能使用机器人，建议使用snode命令。
+    /chart 获取京豆变化数据柱状图和曲线图。例：/chart 1，获取账号1京豆变化。
+    /cmd 执行命令，例：/cmd python3 /python/bot.py，则执行python目录下的bot.py。不建议使用BOT使用并发，可能产生不明原因的崩溃。 
+    /dl 下载文件
+    /edit 从/jd目录选择文件并编辑，需要将编辑好信息全部发给BOT，BOT会根据你发的信息进行替换。建议仅编辑config或crontab.list，其他文件慎用！！！
+    /getcookie 扫码获取cookie，30s内可取消，31s~2分钟内不能进行其他交互直到超时或获取到cookie。
+    /getfile 获取/jd目录下文件。
+    /log 查看脚本执行日志。
+    /node 执行js脚本，输入/node jd_bean_change。如执行非scripts目录js，需输入绝对路径执行。node命令会等待脚本执行完，期间不能使用BOT，建议使用snode命令。
+    /set 设置。
     /setshort 设置自定义按钮，每次设置会覆盖原设置。
-    /snode 命令可以选择脚本执行，只能选择/scripts 和/own目录下的脚本，选择完后直接后台运行，不影响机器人响应其他命令。 
-    /set 设置
-    /dl 下载
-    此外直接发送文件，会让您选择保存到哪个文件夹，如果选择运行，将保存至own目录下，并立即运行脚本'''
+    /snode 选择脚本执行，只能选择/scripts和/own目录下的脚本，选择完后直接后台运行，不影响BOT响应其他命令。 
+    
+    此外，直接发送文件至BOT，会让您选择保存到目标文件夹，支持保存并运行。'''
     await jdbot.send_message(chat_id, msg)
