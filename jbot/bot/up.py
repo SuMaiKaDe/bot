caf7cc9405c170cab5c01e3937eb58f32bb9ef59
SuMaiKaDe <hbtsss@163.com>
2021-06-22 11:03:37 +0800

    修复ver两条消息，修改up命令为远程获取bot.sh

--- jbot/bot/up.py
+++ jbot/bot/up.py
@@ -1,12 +1,22 @@
 from telethon import events
-from .. import jdbot, chat_id, _ConfigDir
+from .. import jdbot, chat_id, _ConfigDir, logger
 from .utils import cmd
+from .update import version, botlog
+import requests
 
 @jdbot.on(events.NewMessage(from_users=chat_id, pattern='^/up$'))
-async def myup(event):
+async def my_up(event):
     try:
-        msg = await jdbot.send_message(chat_id,'开始更新程序，请稍候，\n提示开始运行后，机器人会被杀死，并重新启动\n因此后边更新信息不会再推送，不代表机器人不理你了')
+        msg = await jdbot.send_message(chat_id, '开始更新程序，请稍候，\n提示开始运行后，机器人会被杀死，并重新启动\n因此后边更新信息不会再推送，不代表机器人不理你了')
+        res = requests.get('https://ghprxoy.com/https://raw.githubusercontent.com/SuMaiKaDe/bot/main/config/bot.sh')
+        with open(f'{_ConfigDir}/bot.sh','w+',encoding='utf-8') as f:
+            f.write(res)
         await cmd(f'bash {_ConfigDir}/bot.sh')
-        await jdbot.delete_messages(chat_id,msg)
+        await jdbot.delete_messages(chat_id, msg)
     except Exception as e:
-        await jdbot.send_message(chat_id,str(e))
+        await jdbot.send_message(chat_id, str(e))
+
+
+@jdbot.on(events.NewMessage(from_users=chat_id, pattern=r'^/ver$', incoming=True))
+async def my_ver(event):
+    await jdbot.send_message(chat_id, f'当前版本\n{version}\n{botlog}')
--- jbot/bot/update.py
+++ jbot/bot/update.py
@@ -1,22 +1,11 @@
-from telethon import events
-from .. import jdbot, chat_id
-
-
 version = 'version :0.9.9.6'
 botlog = '''
 **2021年6月21日**
 本次更新内容如下:
-    - 新增 /ver 用来查看当前版本号
     - 青龙新增 /env 命令，用来管理环境变量
         - 使用方法 /env keywords 将列出所有包含keywords的环境变量
     - 青龙新增 /addenv 命令，用来新增环境变量
         - 使用方法 /addenv 触发，按照格式复制修改发送。
         - 以“-->”进行名称、值、备注区隔
-
 '''
 
-
-# @jdbot.on(events.NewMessage(from_users=chat_id, pattern=r'^版本$'))
-@jdbot.on(events.NewMessage(from_users=chat_id, pattern=r'^/ver$'))
-async def my_ver(event):
-    await jdbot.send_message(chat_id, f'当前版本\n{version}\n{botlog}')
