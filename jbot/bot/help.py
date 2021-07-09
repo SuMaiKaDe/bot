15aa8717062d549cd564bd8c1bb6fc8a0c6ceec4
SuMaiKaDe <hbtsss@163.com>
2021-07-09 19:37:05 +0800

    Update help.py

--- jbot/bot/help.py
+++ jbot/bot/help.py
@@ -21,8 +21,7 @@ cmd-执行cmd命令
 edit-编辑文件
 env-管理环境变量
 dl-下载文件
-getcookie-扫码获取cookie
-getfile-获取jd目录下文件
+getfile-获取目录下文件
 log-选择日志
 node-执行js脚本文件，绝对路径
 set-BOT设置
@@ -32,11 +31,10 @@ snode-选择脚本后台运行
 start-开始使用本程序'''
     bean = '/bean 加数字，获取该账户近期收支情况\n/bean in\out获取所有账户近期收或支情况\n/bean 获取账户总豆数量'
     cmd = '/cmd用于执行cmd命令，如果命令持续10分钟仍未结束，将强行终止，以保障机器人响应'
-    edit = '/edit 进入/jd目录选择文件进行编辑，仅限简易编辑\n/edit /jd/config进入config目录选择文件编辑\n/edit /jd/config/config.sh 直接编辑config.sh文件'
-    getcookie = '/getcookie 扫码获取jdcookie'
+    edit = '/edit 进入/jd目录选择文件进行编辑，仅限简易编辑\n/edit /xx/config进入config目录选择文件编辑\n/edit /xx/config/config.sh 直接编辑config.sh文件'
     node = '/node 用于执行js脚本 用法：\n/node /jd/own/abc/def.js'
-    getfile = '/getfile 进入/jd目录选择文件进行获取\n/getfile /jd/config进入config目录选择文件获取\n/getfile /jd/config/config.sh 直接获取config.sh文件'
-    setshort = '/setshort 用于设置快捷方式，格式如下：\n更新-->jup\nAAA-->BBB这种格式使用/a选择\n/bean 1\n/edit /jd/config/config.sh\n以“/”开头的为机器人命令快捷，使用/b选择'
+    getfile = '/getfile 进入/jd目录选择文件进行获取\n/getfile /xx/config进入config目录选择文件获取\n/getfile /xx/config/config.sh 直接获取config.sh文件'
+    setshort = '/setshort 用于设置快捷方式，格式如下：\n更新-->jup\nAAA-->BBB这种格式使用/a选择\n/bean 1\n/edit /xx/config/config.sh\n以“/”开头的为机器人命令快捷，使用/b选择'
     snode = '/snode 选择脚本并运行'
     chart = '/chart 加数字，统计该账户近期收支情况'
     botset = '''/set 
